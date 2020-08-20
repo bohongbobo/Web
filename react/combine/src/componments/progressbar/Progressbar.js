10142cf317b26c629f0e82e72eb2f626c5f9e2f1
Bohong Li <bohongbobo@gmail.com>
2020-08-20 15:45:35 -0400

    update

--- react/combine/src/componments/progressbar/Progressbar.js
+++ react/combine/src/componments/progressbar/Progressbar.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Progressbar extends Component {
-  render() {
-    const style = {
-      height: "30px",
-      width: `${this.props.progress}%`,
-      backgroundColor: "cyan",
-      borderRadius: "5px",
-      transition: "width 0.1s ease-in-out",
-    };
-    return <div style={style}></div>;
-  }
-}
+const Progressbar = () => {
+  const style = {
+    height: "30px",
+    width: `${this.props.progress}%`,
+    backgroundColor: "cyan",
+    borderRadius: "5px",
+    transition: "width 0.1s ease-in-out",
+  };
+  return <div style={style}></div>;
+};
+
+export default Progressbar;
